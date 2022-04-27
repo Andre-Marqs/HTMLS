23b4760d48422a8bcd2c7a5022d00334401effa1
Andre-Marqs <69987560+Andre-Marqs@users.noreply.github.com>
2022-04-27 19:38:20 -0300

    Add files via upload

---
+++ CriandoPrimeiroHTML.js
@@ -0,0 +1,15 @@
+let numeroinput1 = document.getElementById("numero1")
+let numeroinput2 = document.getElementById("numero2")
+let Mostraresultado
+
+
+
+
+function contaSoma(){
+    
+    Mostraresultado = Number(numeroinput1.value) + Number (numeroinput2.value)
+
+    alert (Mostraresultado)
+
+
+}
